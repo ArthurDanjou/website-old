a34f15aa0b3e8d0d5c0ba014df07dae88602bef2
Arthur DANJOU <arthurdanjou@outlook.fr>
2020-10-29 23:54:29 +0100

    remove build dir

--- build/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Footer } from '../..\\src\\components\\Footer.vue'
-export { default as Header } from '../..\\src\\components\\Header.vue'
-export { default as HomePost } from '../..\\src\\components\\HomePost.vue'
-export { default as Post } from '../..\\src\\components\\Post.vue'
-
-export const LazyFooter = import('../..\\src\\components\\Footer.vue' /* webpackChunkName: "components_Footer" */).then(c => c.default || c)
-export const LazyHeader = import('../..\\src\\components\\Header.vue' /* webpackChunkName: "components_Header" */).then(c => c.default || c)
-export const LazyHomePost = import('../..\\src\\components\\HomePost.vue' /* webpackChunkName: "components_HomePost" */).then(c => c.default || c)
-export const LazyPost = import('../..\\src\\components\\Post.vue' /* webpackChunkName: "components_Post" */).then(c => c.default || c)
