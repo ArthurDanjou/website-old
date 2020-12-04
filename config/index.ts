f8a030fb3df8876e172ba0ae2679b077f4bdae98
Arthur DANJOU <arthurdanjou@outlook.fr>
2020-12-04 10:56:38 +0100

    Upgrading yarn

--- config/index.ts
+++ config/index.ts
@@ -3,5 +3,6 @@ import Head from './Head'
 import ColorMode from './ColorMode'
 import Tailwind from './Tailwind'
 import Translation from './Translation'
+import Ngrok from './Ngrok'
 
-export { Axios, Head, ColorMode, Tailwind, Translation }
+export { Axios, Head, ColorMode, Tailwind, Translation, Ngrok }
