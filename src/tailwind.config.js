cd842c0abb2bad63115aad7eed954a069c2f1d37
Arthur DANJOU <arthurdanjou@outlook.fr>
2020-12-04 10:37:30 +0100

    Upgrading yarn

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,13 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   presets: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
