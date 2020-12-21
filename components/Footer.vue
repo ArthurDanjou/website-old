45b24cf799a46027a6fa91bd71f293b891ad9f5a
Arthur DANJOU <arthurdanjou@outlook.fr>
2020-12-21 12:33:07 +0100

    Improve SEO

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,21 +5,21 @@
         <div class="mb-3">
           <p class="inline">{{ $t('footer.find_me') }}
             <br class="md:hidden"/>
-            <a class="font-semibold" href="https://twitch.com/ArthurDanjou" target="_blank">
+            <a class="font-semibold" href="https://twitch.com/ArthurDanjou" target="_blank" rel="noopener noreferrer">
               <img class="inline img" src="@/assets/img/socials/twitch.svg" alt="Twitch logo" height="20" width="20" />
               <span class="link">Twitch</span>
             </a>,
-            <a class="font-semibold" href="https://github.com/ArthurDanjou" target="_blank">
+            <a class="font-semibold" href="https://github.com/ArthurDanjou" target="_blank" rel="noopener noreferrer">
               <img class="inline img" src="@/assets/img/socials/github.svg" alt="Github logo" height="20" width="20" />
               <span class="link">Github</span>
             </a> &
-            <a class="font-semibold" href="https://twitter.com/ArthurDanj" target="_blank">
+            <a class="font-semibold" href="https://twitter.com/ArthurDanj" target="_blank" rel="noopener noreferrer">
               <img class="inline img" src="@/assets/img/socials/twitter.svg" alt="Twitter logo" height="20" width="20" />
               <span class="link">Twitter</span>
             </a>
             <br class="md:hidden"/>
             {{ $t('footer.separator') }}
-            <a class="font-semibold" href="mailto:contact@arthurdanjou.fr" target="_blank">
+            <a class="font-semibold" href="mailto:contact@arthurdanjou.fr" target="_blank" rel="noopener noreferrer">
               <svg class="inline img" width="20" height="20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
@@ -33,12 +33,12 @@
         </div>
         <p>
           {{ $t('footer.credits') }}
-          <a class="font-semibold" target="_blank" href="https://nuxtjs.org">
+          <a class="font-semibold" target="_blank" href="https://nuxtjs.org" rel="noopener noreferrer">
             <img class="inline img" src="@/assets/img/socials/nuxtjs.svg" alt="NuxtJs logo" height="20" width="20" />
             <span class="link">NuxtJs</span>
           </a>
           &
-          <a class="font-semibold" target="_blank" href="https://preview.adonisjs.com/">
+          <a class="font-semibold" target="_blank" href="https://preview.adonisjs.com/" rel="noopener noreferrer">
             <img class="inline img" src="@/assets/img/socials/adonisjs.svg" alt="AdonisJs logo" height="20" width="20" />
             <span class="link">AdonisJs</span>
           </a>
