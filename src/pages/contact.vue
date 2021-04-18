8818f5213a07c66b4438ceb8872b67fca2c88021
Arthur Danjou <arthurdanjou@outlook.fr>
2021-04-18 14:33:48 +0200

    Add default color to hiring status

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -56,7 +56,8 @@ export default {
           return `bg-green-200 text-green-700`
         case 'red':
           return `bg-red-200 text-red-700`
-
+        default:
+          return 'bg-green-200 text-green-700'
       }
     })
 
