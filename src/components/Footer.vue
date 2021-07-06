33248653a442f47c31049a2b3430cb775be0cb17
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-07-06 23:07:02 +0200

    Fix mobile view

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer w-full border-t-2 border-solid border-gray-200 px-4 xl:px-32 dark:border-gray-800 mb-22 md:mb-0">
     <div class="flex flex-col-reverse md:flex-row justify-between pt-4 items-center">
-      <div class="inline flex space-x-4 mb-4 md:mb-0">
+      <div class="inline flex space-x-4">
         <a class="link font-semibold" href="https://twitch.com/ArthurDanjou" target="_blank" rel="noopener noreferrer">
           <TwitchIcon /> <span>Twitch</span>,
         </a>
@@ -15,7 +15,7 @@
           <MailIcon /> <span>Mail</span>
         </a>
       </div>
-      <div class="flex space-x-4">
+      <div class="flex space-x-4 mb-4 md:mb-0">
         <nuxt-link class="link font-semibold" to="/env">
           <span>{{ $t('header.env') }}</span>,
         </nuxt-link>
