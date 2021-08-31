f7d8dda9ce1bec9e1ff15a81806ca3420cb3485c
Arthur DANJOU <arthurdanjou@outlook.fr>
2021-08-31 19:47:29 +0200

    Move v-if

--- src/components/AboutHome.vue
+++ src/components/AboutHome.vue
@@ -1,10 +1,10 @@
 <template>
-  <section v-if="age" class="w-full flex items-center justify-center my-12">
+  <section class="w-full flex items-center justify-center my-12">
     <div class="flex flex-col md:flex-row justify-between items-center">
       <div class="mb-12 md:mb-0 w-full md:w-1/2 flex justify-center">
         <img src="@/assets/images/memojies/Hat.png" alt="It's me !" class="xl:w-1/2" />
       </div>
-      <div class="md:w-1/2 text-justify">
+      <div v-if="age" class="md:w-1/2 text-justify">
         <h2 class="text-4xl font-bold text-center md:text-justify">
           {{ $t('home.about.title') }}
         </h2>
--- src/components/PostsHome.vue
+++ src/components/PostsHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-if="posts" class="w-full flex items-center justify-center my-12">
+  <section class="w-full flex items-center justify-center my-12">
     <div class="flex flex-col items-center text-center">
       <div class="flex flex-col items-center">
         <h2 class="font-bold text-3xl">
@@ -9,7 +9,7 @@
           {{ $t('blog.description') }}
         </p>
       </div>
-      <div class="w-full lg:w-1/2 mt-20 grid grid-cols-1 gap-y-24 mb-8 sm:mb-24 text-left">
+      <div v-if="posts" class="w-full lg:w-1/2 mt-20 grid grid-cols-1 gap-y-24 mb-8 sm:mb-24 text-left">
         <Post
           v-for="post in posts"
           :key="post.slug"
