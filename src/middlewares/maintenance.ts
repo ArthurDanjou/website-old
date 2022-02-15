9ac59dac2c28760cf457d29ec2c7adb871ca68a6
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-02-15 18:00:59 +0100

    Add maintenance test

--- src/middlewares/maintenance.ts
+++ src/middlewares/maintenance.ts
@@ -2,13 +2,17 @@ import {Context} from "@nuxt/types";
 
 export default async function ({redirect, route, $axios}: Context) {
   let isMaintenance = true
-  const response = await $axios.get('/api/maintenance', {
-    headers: {
-      'Authorization': `Bearer ${process.env.API_TOKEN}`
+  try {
+    const response = await $axios.get('/api/maintenance', {
+      headers: {
+        'Authorization': `Bearer ${process.env.API_TOKEN}`
+      }
+    })
+    if (response.status === 200) {
+      isMaintenance = response.data.maintenance.active === 1
     }
-  })
-  if (response.status === 200) {
-    isMaintenance = response.data.maintenance.active === 1
+  } catch (error) {
+    return redirect('/maintenance')
   }
   if (isMaintenance) {
     return redirect('/maintenance')
